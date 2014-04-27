3e88f611a7d089094e430614a5ee8e8f990f9865
jbhatab <jbhatab@gmail.com>
2014-04-26 20:09:57 -0500

    heroku fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,8 @@ Inspector::Application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.assets.compile = true
+
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
